c1b0dddad7f175c4ebe1d243519e704452ef6007
Ripan Baidya <ripanbaidya7980@gmail.com>
2025-08-22 23:41:19 +0530

    add email template, template: thymleaf

---
+++ src/main/java/org/astrobrains/email/EmailTemplateName.java
@@ -0,0 +1,16 @@
+package org.astrobrains.email;
+
+import lombok.Getter;
+
+@Getter
+public enum EmailTemplateName {
+
+    ACTIVATE_ACCOUNT("activate-account");
+
+    private final String name;
+
+
+    EmailTemplateName(String name) {
+        this.name = name;
+    }
+}
