111cf98bd1efd3e1c59f789d98f7d45a5176da1d
Ripan Baidya <ripanbaidya7980@gmail.com>
2025-08-22 23:45:33 +0530

    update filename: Application

--- src/main/java/org/astrobrains/Application.java
+++ src/main/java/org/astrobrains/Application.java
@@ -1,17 +1,34 @@
 package org.astrobrains;
 
+import org.astrobrains.repository.RoleRepository;
+import org.astrobrains.role.Role;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAsync
-public class AuthXApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthXApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
+    /**
+     * When start the application, if user does
+     */
+    @Bean
+    public CommandLineRunner runner(RoleRepository roleRepository) {
+        return arts -> {
+            if (roleRepository.findByName("USER").isEmpty()) {
+                roleRepository.save(
+                        Role.builder().name("USER").build()
+                );
+            }
+        };
+    }
 }
